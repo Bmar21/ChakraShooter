2979d848123f34c43b06d7b6ad65743643b17320
Bmar21 <brandonmalor21@gmail.com>
2017-06-01 13:24:23 -0600

    Made the Ship animate and setup data members for background

--- ChakraShooter/Framework/Model/Player.cs
+++ ChakraShooter/Framework/Model/Player.cs
@@ -1,42 +1,50 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using ChakraShooter.View;
 
 namespace ChakraShooter.Model
 {
 	public class Player
 	{
-		public void Initialize(Texture2D texture, Vector2 position)
+		// Initialize the player
+		public void Initialize(Animation animation, Vector2 position)
 		{
-			PlayerTexture = texture;
+			playerAnimation = animation;
 
-			// Set the starting position of the player around the middle of the screen and to the back 
+			// Set the starting position of the player around the middle of the screen and to the back
 			Position = position;
 
-			// Set the player to be active 
+			// Set the player to be active
 			Active = true;
 
 			// Set the player health
 			Health = 100;
 		}
 
-		public void Update()
-		{
-		}
-
+		// Draw the player
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			playerAnimation.Draw(spriteBatch);
+		}
+
+		// Update the player animation
+		public void Update(GameTime gameTime)
+		{
+			playerAnimation.Position = Position;
+			playerAnimation.Update(gameTime);
 		}
 
 
 		// Animation representing the player
-		private Texture2D playerTexture;
-		public Texture2D PlayerTexture
+		private Animation playerAnimation;
+		public Animation PlayerAnimation
 		{
-			get { return playerTexture; }
-			set { playerTexture = value; }
+			get { return playerAnimation; }
+			set { playerAnimation = value; }
 		}
+
 
 		// Position of the Player relative to the upper left side of the screen
 		// As a struct it cannot be used as a property 😢 sad panda
@@ -61,13 +69,13 @@ namespace ChakraShooter.Model
 		// Get the width of the player ship
 		public int Width
 		{
-			get { return PlayerTexture.Width; }
+			get { return playerAnimation.FrameWidth; }
 		}
 
 		// Get the height of the player ship
 		public int Height
 		{
-			get { return PlayerTexture.Height; }
+			get { return playerAnimation.FrameHeight; }
 		}
 
 	}
---
+++ ChakraShooter/Framework/View/ParallaxingBackground.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChakraShooter
+{
+	public class ParallaxingBackground
+	{
+		// The image representing the parallaxing background
+		private Texture2D texture;
+
+		// An array of positions of the parallaxing background
+		private Vector2[] positions;
+
+		// The speed which the background is moving
+		private int speed;
+	}
+
+	public void Initialize(ContentManager content, String texturePath, int screenWidth, int speed)
+	{
+		// Load the background texture we will be using
+		texture = content.Load(texturePath);
+
+		// Set the speed of the background
+		this.speed = speed;
+
+		// If we divide the screen with the texture width then we can determine the number of tiles need.
+		// We add 1 to it so that we won't have a gap in the tiling
+		positions = new Vector2[screenWidth / texture.Width + 1];
+
+		// Set the initial positions of the parallaxing background
+		for (int i = 0; i < positions.Length; i++)
+		{
+			// We need the tiles to be side by side to create a tiling effect
+			positions[i] = new Vector2(i * texture.Width, 0);
+		}
+	}
+}
