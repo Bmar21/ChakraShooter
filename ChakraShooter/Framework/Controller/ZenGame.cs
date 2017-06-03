187831e301f9cdd452b7871b342503ce0c3dd18d
Bmar21 <brandonmalor21@gmail.com>
2017-06-03 02:43:47 -0600

    added game music, laser, and explotion sounds

--- ChakraShooter/Framework/Controller/ZenGame.cs
+++ ChakraShooter/Framework/Controller/ZenGame.cs
@@ -67,6 +67,15 @@ namespace ChakraShooter.Controller
 		private Texture2D explosionTexture;
 		private List<Animation> explosions;
 
+        // The sound that is played when a laser is fired
+        private SoundEffect laserSound;
+
+        // The sound used when the player or an enemy dies
+        private SoundEffect explosionSound;
+
+        // The music played during gameplay
+        private Song gameplayMusic;
+
 		/// <summary>
 		/// Allows the game to perform any initialization it needs to before starting to run.
 		/// This is where it can query for any required services and load any non-graphic
@@ -125,6 +134,15 @@ namespace ChakraShooter.Controller
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 			player.Initialize(playerAnimation, playerPosition);
 
+            // Load the music
+            gameplayMusic = Content.Load<Song>("Sound/gameMusic");
+
+            // Load the laser and explosion sound effect
+            laserSound = Content.Load<SoundEffect>("Sound/laserFire");
+            explosionSound = Content.Load<SoundEffect>("Sound/explosion");
+
+            // Start the music right away
+            PlayMusic(gameplayMusic);
 
 			// Load the parallaxing background
 			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
@@ -274,6 +292,8 @@ namespace ChakraShooter.Controller
 				// Add the projectile, but add it to the front and center of the player
 				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
 
+                // Play the laser sound
+                laserSound.Play();
 			}
 
 		}
@@ -322,8 +342,12 @@ namespace ChakraShooter.Controller
                     {
                         // Add an explosion
                         AddExplosion(enemies[i].Position);
+                        // Play the explosion sound
+                        explosionSound.Play();
                     }
+
 					enemies.RemoveAt(i);
+
 				}
 			}
 
@@ -424,7 +448,20 @@ namespace ChakraShooter.Controller
                 }
             }
         
+        private void PlayMusic(Song song)
+        {
+            // Due to the way the MediaPlayer plays music,
+            // we have to catch the exception. Music will play when the game is not tethered
+            try
+            {
+                // Play the music
+                MediaPlayer.Play(song);
 
+                // Loop the currently playing song
+                MediaPlayer.IsRepeating = true;
+            }
+            catch { } //No Exception is handled so it is an empty/anonymous exception
+        }
 
 	}
 }
